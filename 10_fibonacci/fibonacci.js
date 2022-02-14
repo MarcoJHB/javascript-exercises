5ece794b74cf5ed1e0338973d72351ecd383a707
MarcoJHB <marcothepolo@gmail.com>
2022-02-14 20:44:25 +0200

    Finished fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function (str) {
+  let number = parseInt(str);
 
+  // prevent 1 from returning undefined
+  if (number == 1) {
+    return 1;
+  }
+  // prevent negative numbers
+  if (number < 0) {
+    return "OOPS";
+  }
+
+  //fibonacci sequence
+  let n1 = 0,
+    n2 = 1,
+    nextTerm;
+
+  for (let i = 1; i < number; i++) {
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+  }
+
+  return nextTerm;
 };
+
+// console.log(fibonacci("1"));
 
 // Do not edit below this line
 module.exports = fibonacci;
