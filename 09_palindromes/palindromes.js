b97d88e085a031f28bfb350cffcfb4e067f6454c
MarcoJHB <marcothepolo@gmail.com>
2022-02-14 20:44:08 +0200

    Finished Palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let forwardPhrase = Array.from(str.toLowerCase().replace(/[^a-zA-Z ]/g, "").replace(/\s+/g, ''));
+    // console.log(forwardPhrase);
+    // console.log(forwardPhrase[forwardPhrase.length-1]);
 
+    for (let i = 0; i< forwardPhrase.length / 2; i++) {
+        if (forwardPhrase[i] !== forwardPhrase[forwardPhrase.length-1-i]) {
+            // console.log("doesn't work");
+            return false;
+        }
+    }
+    return true;
 };
+
+// console.log(palindromes("RACECAR"));
 
 // Do not edit below this line
 module.exports = palindromes;
