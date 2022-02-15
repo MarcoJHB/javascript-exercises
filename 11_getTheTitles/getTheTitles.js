f4c540ae27037d3f5f8c63e4b8deb1cc4e15fde3
MarcoJHB <marcothepolo@gmail.com>
2022-02-15 07:53:50 +0200

    Added in map() to simplify function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+
+  //solution 1  
+//   let bookArr = [];
+
+//   for (let i = 0; i < books.length; i++) {
+//     bookArr.push(books[i].title);
+//   }
+
+//   return bookArr;
+
+    //solution 2
+    return books.map(x => x['title']);
 
 };
+
+// console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
